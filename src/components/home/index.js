63aed0c1a57203a78a31840fbece79451d012288
mehmethalis <mehmethaliscicek@gmail.com>
2022-12-22 15:55:47 +0300

    removed tables

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./home.css";
 import Navbar from "./Navbar";
-import Tables from "./Tables";
 
 export default function Home() {
   return (
     <div className="site-statistic-demo-card">
       <Navbar />
-      <Tables />
     </div>
   );
 }
