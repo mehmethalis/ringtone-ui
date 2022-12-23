6c63427718b140c79767b43cece503d436458040
mehmethalis <mehmethaliscicek@gmail.com>
2022-12-23 20:45:45 +0300

    update source of player

--- src/components/shared/player/index.js
+++ src/components/shared/player/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import AudioPlayer from "react-h5-audio-player";
-import { RocketOutlined } from "@ant-design/icons";
 import { Col, Row } from "antd";
 import "./player.css";
 import CDIcon from "../../../assets/images/cd.png";
+import Sarkı from "../../../assets/ringtones/ringones_short/bird_chirp_short.mp3";
 
 export default function Player() {
   return (
@@ -11,14 +11,14 @@ export default function Player() {
       <Row justify={"center"}>
         <Col span={4}>
           <div className="cover">
-            <img src={CDIcon} alt="cd icon" width={30}/>
+            <img src={CDIcon} alt="cd icon" width={30} />
             <p className="caption">Şarkı 1</p>
           </div>
         </Col>
         <Col span={20}>
           <AudioPlayer
             autoPlay
-            src="https://diviextended.com/wp-content/uploads/2021/10/sound-of-waves-marine-drive-mumbai.mp3"
+            src={Sarkı}
             onPlay={(e) => console.log("onPlay")}
             // other props here
           />
