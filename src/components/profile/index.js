8ebf5b593fc877e7d52e9efe301b27cb54a7035d
mehmethalis <mehmethaliscicek@gmail.com>
2022-12-25 14:26:38 +0300

    ucuz yırttık

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,11 +1,15 @@
-import { Avatar, Card, Row, Col } from "antd";
+import { Avatar, Card, Row, Col, Badge } from "antd";
 import React from "react";
 import "./profile.css";
 import { EditOutlined, SettingOutlined } from "@ant-design/icons";
 import SongsList from "../shared/songsList";
+import { useSelector } from "react-redux";
+
 const { Meta } = Card;
 
 export default function Profile() {
+  const { username, userRingtones } = useSelector((state) => state.userState);
+
   return (
     <div className="site-statistic-demo-card">
       <Row>
@@ -15,15 +19,25 @@ export default function Profile() {
               width: 300,
               height: 150,
             }}
-            actions={[
-              <SettingOutlined key="setting" />,
-              <EditOutlined key="edit" />,
-            ]}
+            actions={[<SettingOutlined key="setting" />, <EditOutlined key="edit" />]}
           >
             <Meta
-              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-              title="Name Surname"
-              description="username"
+              avatar={
+                <Avatar
+                  style={{ backgroundColor: "#7265e6", verticalAlign: "middle" }}
+                  size={60}
+                  gap={4}
+                >
+                  {username}
+                </Avatar>
+              }
+              title="John Doe"
+              description={
+                <span>
+                  <sm style={{ fontSize: "10px" }}>Downloadable</sm>{" "}
+                  <Badge color="#7265e6" size="medium" count={userRingtones ? "+" + userRingtones.length : "0"} />
+                </span>
+              }
             />
           </Card>
         </Col>
