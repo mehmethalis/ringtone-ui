357f1d66155c7976e50aba513e419820ad2a73d4
Mehmet Halis <69078217+mehmethalis@users.noreply.github.com>
2022-12-25 14:25:48 +0300

    Revert "new pr (#11)"
    This reverts commit a89eca4b9cab5f47a8132e7ccfab7ee9a74b0514.

--- src/components/shared/songsList/index.js
+++ src/components/shared/songsList/index.js
@@ -1,61 +1,89 @@
-import React, { useEffect, useState } from "react";
-import { List, Button } from "antd";
+import React from "react";
+import { List, Button, Badge } from "antd";
 import {
   DownloadOutlined,
   ShoppingOutlined,
   PlayCircleOutlined,
+  FieldTimeOutlined,
+  CopyrightOutlined,
+  LogoutOutlined,
 } from "@ant-design/icons";
 import VirtualList from "rc-virtual-list";
 import Logo from "../logo";
 import "./songslist.css";
+import { useDispatch, useSelector } from "react-redux";
+import { selectSong } from "../../../store/actions/player.action";
+import { addItem, removeItem } from "../../../store/actions/user.actions";
 
-const fakeDataUrl =
-  "https://randomuser.me/api/?results=20&inc=name,gender,email,nat,picture&noinfo";
-const ContainerHeight = 400;
-export default function SongsList({isDownload, songs}) {
-  const [data, setData] = useState([]);
-  const appendData = () => {
-    fetch(fakeDataUrl)
-      .then((res) => res.json())
-      .then((body) => {
-        setData(data.concat(body.results));
-      });
-  };
-  useEffect(() => {
-    appendData();
-  }, []);
-  const onScroll = (e) => {
-    if (
-      e.currentTarget.scrollHeight - e.currentTarget.scrollTop ===
-      ContainerHeight
-    ) {
-      appendData();
-    }
-  };
+export default function SongsList({ isDownload, songs, isLoading, isPreview }) {
+  const dispatch = useDispatch();
+  const {
+    basket: { items },
+  } = useSelector((state) => state.userState);
+
   return (
-    <List>
-      <VirtualList
-        data={data}
-        height={ContainerHeight}
-        itemHeight={47}
-        itemKey="email"
-        onScroll={onScroll}
-      >
+    <List loading={isLoading}>
+      <VirtualList data={songs} itemHeight={47} itemKey={"id"}>
         {(item) => (
-          <List.Item key={item.email}>
+          <List.Item key={item.id} style={{ padding: "15px" }}>
             <List.Item.Meta
-              avatar={<Logo isSmall={true}/>}
-              title={<a href="https://ant.design">{item.name.last}</a>}
-              description={item.email}
+              avatar={<Logo isSmall={true} />}
+              title={
+                <div style={{ width: "260px" }}>
+                  <Badge.Ribbon
+                    text={<b style={{ fontSize: "17px" }}>{"$ " + item.price}</b>}
+                    color="purple"
+                  >
+                    <b>{item.title}</b>
+                  </Badge.Ribbon>
+                </div>
+              }
+              description={
+                <div>
+                  <span>
+                    <FieldTimeOutlined
+                      style={{ fontSize: "18px", padding: "3px", color: "#0f172a" }}
+                    />
+                    {item.duration}
+                  </span>
+                  <span style={{ color: `${item.isCopyright ? "#0f172a" : "#059669"}` }}>
+                    <CopyrightOutlined
+                      style={{
+                        fontSize: "16px",
+                        padding: "3px",
+                        marginLeft: "15px",
+                      }}
+                    />
+                    {item.isCopyright ? "has copyright" : "no copyright"}
+                  </span>
+                </div>
+              }
             />
             <div className="buttons">
-              <Button icon={<PlayCircleOutlined />} />
-              {isDownload && <Button
-                shape="circle"
-                icon={<DownloadOutlined />}
-                size={"default"}
-              />}
-              <Button icon={<ShoppingOutlined />} />
+              <Button
+                icon={<PlayCircleOutlined />}
+                onClick={() =>
+                  dispatch(
+                    selectSong({ selectedSong: item, songType: isPreview ? "isPreview" : "full" })
+                  )
+                }
+              >
+                Preview
+              </Button>
+              {isDownload && <Button shape="circle" icon={<DownloadOutlined />} size={"default"} />}
+              {!isDownload && (
+                <Button
+                  danger={items.includes(item)}
+                  icon={items.includes(item) ? <LogoutOutlined /> : <ShoppingOutlined />}
+                  onClick={
+                    items.includes(item)
+                      ? () => dispatch(removeItem(item))
+                      : () => dispatch(addItem(item))
+                  }
+                >
+                  {items.includes(item) ? "Remove" : "Add to Basket"}
+                </Button>
+              )}
             </div>
           </List.Item>
         )}
