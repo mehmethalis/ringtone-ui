995d84ae939587c91244c2a5f3ab217bbe2e0c29
mehmethalis <mehmethaliscicek@gmail.com>
2022-12-25 16:06:58 +0300

    profile page

--- src/components/shared/logo/index.js
+++ src/components/shared/logo/index.js
@@ -1,8 +1,6 @@
 import React from "react";
-import logo from "../../../assets/logo/logo.png"
+import logo from "../../../assets/logo/logo.png";
 
-export default function Logo({isSmall}){
-    return(
-        <img className="logo" src={logo} alt='ringtone logo' width={isSmall ? 50 : 80}></img>
-    );
+export default function Logo({ isSmall }) {
+  return <img className="logo" src={logo} alt="ringtone logo" width={isSmall ? 50 : 80}></img>;
 }
--- src/components/shared/songsList/index.js
+++ src/components/shared/songsList/index.js
@@ -14,12 +14,26 @@ import "./songslist.css";
 import { useDispatch, useSelector } from "react-redux";
 import { selectSong } from "../../../store/actions/player.action";
 import { addItem, removeItem } from "../../../store/actions/user.actions";
+import { toast } from "react-toastify";
 
 export default function SongsList({ isDownload, songs, isLoading, isPreview }) {
   const dispatch = useDispatch();
   const {
     basket: { items },
   } = useSelector((state) => state.userState);
+
+  const handleDownload = (item) => {
+    const file = isPreview
+      ? require(`../../../assets/ringtones/ringtones_short/${item.fileName}`)
+      : require(`../../../assets/ringtones/ringtones_long/${item.fileName}`);
+
+    toast.success("Zil sesiniz başarıyla indirildi!");
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = file;
+    a.download = item.title + ".mp3";
+    a.click();
+  };
 
   return (
     <List loading={isLoading}>
@@ -30,12 +44,16 @@ export default function SongsList({ isDownload, songs, isLoading, isPreview }) {
               avatar={<Logo isSmall={true} />}
               title={
                 <div style={{ width: "260px" }}>
-                  <Badge.Ribbon
-                    text={<b style={{ fontSize: "17px" }}>{"$ " + item.price}</b>}
-                    color="purple"
-                  >
+                  {isDownload ? (
                     <b>{item.title}</b>
-                  </Badge.Ribbon>
+                  ) : (
+                    <Badge.Ribbon
+                      text={<b style={{ fontSize: "17px" }}>{"$ " + item.price}</b>}
+                      color="purple"
+                    >
+                      <b>{item.title}</b>
+                    </Badge.Ribbon>
+                  )}
                 </div>
               }
               description={
@@ -70,7 +88,14 @@ export default function SongsList({ isDownload, songs, isLoading, isPreview }) {
               >
                 Preview
               </Button>
-              {isDownload && <Button shape="circle" icon={<DownloadOutlined />} size={"default"} />}
+              {isDownload && (
+                <Button
+                  shape="circle"
+                  icon={<DownloadOutlined />}
+                  size={"default"}
+                  onClick={() => handleDownload(item)}
+                />
+              )}
               {!isDownload && (
                 <Button
                   danger={items.includes(item)}
