34e24d95991ffebbaba3305d418f3b701c99de93
elifcumbul <elifcumbul@gmail.com>
2022-12-25 15:37:38 +0300

    ringtones icon

--- src/components/layout/DefaultHeader.js
+++ src/components/layout/DefaultHeader.js
@@ -4,7 +4,6 @@ import {
   LogoutOutlined,
   CrownFilled,
   ShoppingCartOutlined,
-  CloseOutlined,
 } from "@ant-design/icons";
 import { Layout, Avatar, Popover, Badge, theme, Drawer, Button } from "antd";
 import "./layout.css";
@@ -64,6 +63,7 @@ export default function DefaultHeader({ bread }) {
             open={open}
             footer={<DrawerFooter/>}
           >
+            <DrawerBody/>
           </Drawer>
         </Badge>
 
